1cb103f13d74d41435915e5b2d19159f7e9ae037
Steven G. Johnson <stevenj@alum.mit.edu>
2014-02-07 19:40:43 -0500

    clear_output support (not working yet) for #134

--- src/execute_request.jl
+++ src/execute_request.jl
@@ -210,3 +210,15 @@ function execute_request_0x535c5df2(socket, msg)
 end
 
 #######################################################################
+
+# The user can call IJulia.clear_output() to clear visible output from the
+# front end, useful for simple animations.  Using wait=true clears the
+# output only when new output is available, for minimal flickering.
+function clear_output(wait=false)
+    # flush pending stdio
+    flush_cstdio() # flush writes to stdout/stderr by external C code   
+    send_stream(takebuf_string(read_stdout.buffer), "stdout")
+    send_stream(takebuf_string(read_stderr.buffer), "stderr")
+    send_ipython(publish, msg_reply(execute_msg::Msg, "clear_output",
+                                    ["wait" => wait]))
+end
