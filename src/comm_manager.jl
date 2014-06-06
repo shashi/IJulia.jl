9a2154b8cd1aeb72c8687e3ad3c73e6eec8c366f
Shashi Gowda <shashigowda91@gmail.com>
2014-06-06 21:08:39 +0530

    Fixes. Tested the hypothetical code in comm_manager

--- src/comm_manager.jl
+++ src/comm_manager.jl
@@ -1,6 +1,9 @@
 module CommManager
 
 using IJulia
+
+import IJulia: Msg, uuid4, send_ipython, publish, execute_msg, msg_pub
+
 export Comm, msg_comm, send_comm, close_comm, on_msg,
        on_close, register_target, comm_msg, comm_open, comm_close
 
@@ -15,11 +18,11 @@ function Comm(t)
     # Create a new primary Comm with target t
     comm = Comm{symbol(t)}(uuid4(), true)
     # Is this the right parent for a comm_open?
-    global execute_msg
+    global execute_msg, publish
     # Create a secondary Comm object in the frontend
-    send_ipython(publish,
-                 msg_comm(comm, execute_msg, "comm_open",
-                          data=["target_name"=>string(t)]))
+    send_ipython(IJulia.publish,
+                 msg_comm(comm, IJulia.execute_msg, "comm_open",
+                          Dict(), target_name=string(t)))
     comms[comm.id] = comm
     return comm
 end
@@ -40,37 +43,37 @@ function msg_comm(comm::Comm, m::IJulia.Msg, msg_type,
                "data"=>data]
 
     for (k, v) in kwargs
-        content[k] = v
+        content[string(k)] = v
     end
 
-    return msg_pub(m, msg_type, content,
-                   metadata=metadata)
+    return msg_pub(m, msg_type, content, metadata)
 end
 
 
 function send_comm(comm::Comm, data::Dict,
                    metadata::Dict = Dict(); kwargs...)
-    parent = get!(parents, comm, execute_msg)
+    global publish
+    parent = get!(parents, comm, IJulia.execute_msg)
     msg = msg_comm(comm, parent, "comm_msg", data,
                    metadata; kwargs...)
-    send_ipython(publish, msg)
+    send_ipython(IJulia.publish, msg)
     parents[comm] = msg
 end
 
 
 function close_comm(comm::Comm, data::Dict = Dict(),
                     metadata::Dict = Dict(); kwargs...)
-    parent = get!(parents, comm, execute_msg)
+    parent = get!(parents, comm, IJulia.execute_msg)
+    global publish
     msg = msg_comm(comm, parent, "comm_msg", data,
                    metadata; kwargs...)
-    send_ipython(publish, msg)
+    send_ipython(IJulia.publish, msg)
     parents[comm] = msg
 end
 
 
 function on_msg(comm::Comm, f::Function)
     comm_msg_handlers[comm] = f
-
 end
 
 function on_close(comm::Comm, f::Function)
@@ -78,7 +81,7 @@ function on_close(comm::Comm, f::Function)
 end
 
 
-function register_comm(comm::Comm, data::Dict = Dict())
+function register_comm(comm::Comm, data)
     # no-op, widgets must override for their targets.
     # Method dispatch on Comm{t} serves
     # the purpose of register_target in IPEP 21.
@@ -88,19 +91,21 @@ end
 # handlers for incoming comm_* messages
 
 function comm_open(sock, msg)
-    if haskey("comm_id", msg.content)
+    if haskey(msg.content, "comm_id")
         comm_id = msg.content["comm_id"]
-        if haskey("target_name", msg.content)
+        if haskey(msg.content, "target_name")
             target = msg.content["target_name"]
-            if ~haskey("data", msg.content)
+            if ~haskey(msg.content, "data")
                 msg.content["data"] = Dict()
             end
+            comm = Comm(symbol(target), comm_id)
             register_comm(comm, msg)
             comms[comm.id] = comm
         else
             # Tear down comm to maintain consistency
             # if a target_name is not present
-            send_ipython(publish,
+            global publish
+            send_ipython(IJulia.publish,
                          msg_comm(Comm(:notarget, comm_id),
                                   msg, "comm_close"))
         end
@@ -109,16 +114,16 @@ end
 
 
 function comm_msg(sock, msg)
-    if haskey("comm_id", msg.content)
+    if haskey(msg.content, "comm_id")
         comm_id = msg.content["comm_id"]
-        if haskey(comm_id, comms)
+        if haskey(comms, comm_id)
             comm = comms[comm_id]
         else
             # We don't have that comm open
             return
         end
-        if haskey(comm, comm_msg_handlers)
-            if ~haskey("data", msg.content)
+        if haskey(comm_msg_handlers, comm)
+            if ~haskey(msg.content, "data")
                 msg.content["data"] = Dict()
             end
             comm_msg_handlers[comm](msg)
@@ -129,12 +134,12 @@ end
 
 
 function comm_close(sock, msg)
-    if haskey("comm_id", msg.content)
+    if haskey(msg.content, "comm_id")
         comm_id = msg.content["comm_id"]
         comm = comms[comm_id]
         if haskey(comm, comm_close_handlers)
-            if ~haskey("data", msg.content)
-                msg.content["data"] = {}
+            if ~haskey(msg.content, "comm_id")
+                msg.content["d ata"] = {}
             end
             comm_close_handlers[comm](msg)
             pop!(comm_close_handlers, comm)
